cd1da1d5aea9ed1a0a937bbda30ad4704288b8c8
AllenKeelig <allen.keeling@atlasschool.com>
2024-12-04 20:43:51 -0600

    1111

--- scripts.js
+++ scripts.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const carousel = document.getElementById("carouselExampleControls");
   const carouselInner = document.getElementById("carousel-quotes");
   fetch("https://smileschool-api.hbtn.info/quotes")
-  .then((response) => response.json())
-  .then((quotes) => {
+    .then((response) => response.json())
+    .then((quotes) => {
     loader.style.display = "none";
     carousel.style.display = "block";
     quotes.forEach((quote, index) => {
@@ -15,20 +15,21 @@ document.addEventListener("DOMContentLoaded", () => {
       <div class="row mx-auto align-items-center">
         <div class="col-12 col-sm-2 col-lg-2 offset-lg-1 text-center">
           <img
-          src="${quote.pic_url}"
-          class="d-block align-self-center"
-          alt="Profile Picture"
+            src="${quote.pic_url}"
+            class="d-block align-self-center rounded-circle"
+            alt="Profile picture of ${quote.name}"
           />
-          </div>
+        </div>
           <div class="col-12 col-sm-7 offset-sm-2 col-lg-9 offset-lg-0">
             <div class="quote-text">
-              <p class="text-white">« ${quote.text}</p>
+              <p class="text-white">« ${quote.text}</p>
               <h4 class="text-white font-weight-bold">${quote.name}</h4>
               <span class="text-white">${quote.title}</span>
             </div>
           </div>
         </div>
-        `;
+      </div>
+      `;
       carouselInner.appendChild(carouselItem);
     });
     $('#carouselExampleControls').carousel();
