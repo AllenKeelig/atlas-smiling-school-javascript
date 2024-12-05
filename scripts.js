bd100096cc83a47b26c04fa6661b82661621c108
AllenKeelig <allen.keeling@atlasschool.com>
2024-12-05 03:55:56 -0600

    some more testing

--- scripts.js
+++ scripts.js
@@ -43,84 +43,84 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Tutorials
 document.addEventListener("DOMContentLoaded", () => {
-  const apiURL = "https://smileschool-api.hbtn.info/popular-tutorials";
-  const carouselInner = document.querySelector("#carouselExampleControls2 .carousel-inner");
-  const loader = document.querySelector(".loader");
+    const apiURL = "https://smileschool-api.hbtn.info/popular-tutorials";
+    const carouselInner = document.querySelector("#carouselExampleControls2 .carousel-inner");
+    const loader = document.querySelector(".loader");
 
-  fetch(apiURL)
-      .then((response) => response.json())
-      .then((tutorials) => {
-          loader.remove();
+    fetch(apiURL)
+        .then((response) => response.json())
+        .then((tutorials) => {
+            loader.remove();
 
-          tutorials.forEach((tutorial, index) => {
-              const cardHTML = `
-                  <div class="carousel-item ${index === 0 ? "active" : ""}">
-                      <div class="card">
-                          <img src="${tutorial.thumb_url}" class="card-img-top" alt="${tutorial.title} thumbnail" />
-                          <div class="card-img-overlay text-center">
-                              <img src="images/play.png" alt="Play" width="64px" height="64px" class="play-overlay mx-auto" />
-                          </div>
-                          <div class="card-body">
-                              <h5 class="card-title font-weight-bold">${tutorial.title}</h5>
-                              <p class="card-text text-muted">${tutorial["sub-title"]}</p>
-                              <div class="creator d-flex align-items-center">
-                                  <img src="${tutorial.author_pic_url}" alt="${tutorial.author}'s profile" width="30px" class="rounded-circle" />
-                                  <h6 class="pl-3 m-0 main-color">${tutorial.author}</h6>
-                              </div>
-                              <div class="info pt-3 d-flex justify-content-between">
-                                  <div class="rating">${generateStars(tutorial.star)}</div>
-                                  <span class="main-color">${tutorial.duration}</span>
-                              </div>
-                          </div>
-                      </div>
-                  </div>
-              `;
-              carouselInner.innerHTML += cardHTML;
-          });
+            tutorials.forEach((tutorial, index) => {
+                const cardHTML = `
+                    <div class="carousel-item ${index === 0 ? "active" : ""}">
+                        <div class="card">
+                            <img src="${tutorial.thumb_url}" class="card-img-top" alt="${tutorial.title} thumbnail" />
+                            <div class="card-img-overlay text-center">
+                                <img src="images/play.png" alt="Play" width="64px" height="64px" class="play-overlay mx-auto" />
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title font-weight-bold">${tutorial.title}</h5>
+                                <p class="card-text text-muted">${tutorial["sub-title"]}</p>
+                                <div class="creator d-flex align-items-center">
+                                    <img src="${tutorial.author_pic_url}" alt="${tutorial.author}'s profile" width="30px" class="rounded-circle" />
+                                    <h6 class="pl-3 m-0 main-color">${tutorial.author}</h6>
+                                </div>
+                                <div class="info pt-3 d-flex justify-content-between">
+                                    <div class="rating">${generateStars(tutorial.star)}</div>
+                                    <span class="main-color">${tutorial.duration}</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                carouselInner.innerHTML += cardHTML;
+            });
 
-          $('#carouselExampleControls2 .carousel-inner').slick({
-              slidesToShow: 4,
-              slidesToScroll: 1,
-              prevArrow: '.popular-left',
-              nextArrow: '.popular-right',
-              responsive: [
-                  {
-                      breakpoint: 1200,
-                      settings: {
-                          slidesToShow: 3,
-                      },
-                  },
-                  {
-                      breakpoint: 768,
-                      settings: {
-                          slidesToShow: 2,
-                      },
-                  },
-                  {
-                      breakpoint: 576,
-                      settings: {
-                          slidesToShow: 1,
-                      },
-                  },
-              ],
-          });
-      })
+            $('#carouselExampleControls2 .carousel-inner').slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                prevArrow: '.popular-left',
+                nextArrow: '.popular-right',
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3,
+                        },
+                    },
+                    {
+                        breakpoint: 768,
+                        settings: {
+                            slidesToShow: 2,
+                        },
+                    },
+                    {
+                        breakpoint: 576,
+                        settings: {
+                            slidesToShow: 1,
+                        },
+                    },
+                ],
+            });
+        })
 
-      .catch((error) => {
-          console.error("Error loading tutorials:", error);
-          loader.textContent = "Failed to load";
-      });
+        .catch((error) => {
+            console.error("Error loading tutorials:", error);
+            loader.textContent = "Failed to load";
+        });
 
-      function generateStars(starCount) {
-          let stars = '<div class="d-flex align-items-center">';
-          for (let i = 0; i < 5; i++) {
-              if (i < starCount) {
-                  stars += '<img src="images/star_on.png" alt="star on" width="15px" />';
-              } else {
-                  stars += '<img src="images/star_off.png" alt="star off" width="15px" />';
-              }
-          }
-          stars += '</div>';
-          return stars;
-      }
+        function generateStars(starCount) {
+            let stars = '<div class="d-flex align-items-center">';
+            for (let i = 0; i < 5; i++) {
+                if (i < starCount) {
+                    stars += '<img src="images/star_on.png" alt="star on" width="15px" />';
+                } else {
+                    stars += '<img src="images/star_off.png" alt="star off" width="15px" />';
+                }
+            }
+            stars += '</div>';
+            return stars;
+        }
 });
